4006e4612b5317e5df181848b4fa4f11df8ce69d
Amjed <manuamjed32@gmail.com>
2023-11-15 20:14:53 +0530

    add notification

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,30 +1,31 @@
-self.addEventListener('install', (event) => {
-    console.log('Service Worker installed');
+self.addEventListener("install", evt => self.skipWaiting());
+ 
+// // (B) CLAIM CONTROL INSTANTLY
+self.addEventListener("activate", evt => self.clients.claim());
+ 
+// (C) LISTEN TO PUSH
+self.addEventListener("push", evt => {
+  const data = evt.data.json();
+  console.log("Push", data);
+  const title = data.title
+  delete data.title
+  self.registration.showNotification(title,data);
 });
 
-self.addEventListener('activate', (event) => {
-    console.log('Service Worker activated');
-});
-
-self.addEventListener('push', (event) => {
-    const options = {
-        body: event.data.text(),
-        icon: 'your-notification-icon.png', // Replace with the URL to your notification icon
-        badge: 'your-notification-badge.png', // Replace with the URL to your notification badge
-    };
-
-    event.waitUntil(
-        self.registration.showNotification('Push Notification', options)
-    );
-});
 
 self.addEventListener('notificationclick', (event) => {
-    event.notification.close();
-    // Add custom logic to handle notification click, e.g., open a specific URL
-    clients.openWindow('https://example.com');
-});
-
-self.addEventListener('notificationclose', (event) => {
-    // Add custom logic to handle notification close, if needed
-    console.log('Notification closed:', event);
-});
+    const notification = event.notification;
+    const action = event.action;
+    console.log(event);
+    console.log(notification);
+    // Determine which action was clicked and perform the corresponding action.
+    if (action === 'action-1') {
+      clients.openWindow(notification.data?.link); 
+    } else{
+      // Handle the "Dismiss" action.
+      notification.close(); // Close the notification.
+    }
+  
+    // You can also do additional tasks here, like sending analytics events or tracking user interactions.
+  });
+  
